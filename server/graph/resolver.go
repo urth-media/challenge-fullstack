c7de39d23d67bf1b102d46068b1a7e4fab19e36c
norbux <norbux@live.com>
2021-11-19 20:12:20 -0600

    GraphQL models setup

---
+++ server/graph/resolver.go
@@ -0,0 +1,13 @@
+package graph
+
+import "service/graph/model"
+
+// This file will not be regenerated automatically.
+//
+// It serves as dependency injection for your app, add any dependencies you require here.
+
+type Resolver struct {
+	items []*model.Item
+	users []*model.User
+	todos []*model.Todo
+}
---
+++ server/graph/schema.resolvers.go
@@ -0,0 +1,66 @@
+package graph
+
+// This file will be automatically regenerated based on the schema, any resolver implementations
+// will be copied through when generating and any unknown code will be moved to the end.
+
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"math/rand"
+	"net/http"
+	"service/graph/generated"
+	"service/graph/model"
+)
+
+func (r *mutationResolver) CreateTodo(ctx context.Context, input model.NewTodo) (*model.Todo, error) {
+	todo := &model.Todo{
+		Text: input.Text,
+		ID:   rand.Int(),
+		User: nil,
+	}
+	r.todos = append(r.todos, todo)
+	return todo, nil
+}
+
+func (r *queryResolver) Items(ctx context.Context) ([]*model.Item, error) {
+	resp, err := http.Get("https://hacker-news.firebaseio.com/v0/topstories.json")
+	checkError(err)
+	defer resp.Body.Close()
+	data, err := ioutil.ReadAll(resp.Body)
+	checkError(err)
+
+	var items []model.Item
+	json.Unmarshal(data, &items)
+
+	for i := range items {
+		r.items = append(r.items, &items[i])
+	}
+
+	return r.items, nil
+}
+
+func (r *queryResolver) Users(ctx context.Context) ([]*model.User, error) {
+	panic(fmt.Errorf("not implemented"))
+}
+
+func (r *queryResolver) Todos(ctx context.Context) ([]*model.Todo, error) {
+	return r.todos, nil
+}
+
+// Mutation returns generated.MutationResolver implementation.
+func (r *Resolver) Mutation() generated.MutationResolver { return &mutationResolver{r} }
+
+// Query returns generated.QueryResolver implementation.
+func (r *Resolver) Query() generated.QueryResolver { return &queryResolver{r} }
+
+type mutationResolver struct{ *Resolver }
+type queryResolver struct{ *Resolver }
+
+func checkError(err error) {
+	if err != nil {
+		log.Println(err)
+	}
+}
